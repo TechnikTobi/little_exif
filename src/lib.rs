f43eeebdbd1f96a8f33ded0335c9f0fad0cd4a1d
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-22 11:39:13 +0200

    Update lib.rs
    Small test for new macro-based EXIF tag enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
+mod exif_tag;
+
 #[cfg(test)]
 mod tests {
+
+    use crate::exif_tag::ExifTag;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+
+	let some: ExifTag = ExifTag::ImageDescription;
+	assert_eq!(some.as_u16(), 0x010e);
+	assert_eq!(some.as_string(), String::from("ImageDescription"));
+	assert_eq!(ExifTag::from_u16(0x010e).unwrap(), ExifTag::ImageDescription);
+	println!("hihi {}", some.as_u16());
     }
 }
