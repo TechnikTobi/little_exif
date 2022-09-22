fa54dbab770dc8cfe64658a7e26aaddb85eb8403
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-22 22:45:48 +0200

    Update endian.rs
    Added endian-based conversions for different types

--- src/endian.rs
+++ src/endian.rs
@@ -1,6 +1,64 @@
+#[derive(PartialEq)]
 pub enum
 Endian
 {
 	Big,
 	Little
+}
+
+pub trait
+U8conversion
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>;
+}
+
+impl U8conversion for u16
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		match *endian
+		{
+			Endian::Little => vec![
+				*self as u8, 
+				(*self >> 8) as u8
+			],
+			Endian::Big => vec![
+				(*self >> 8) as u8,
+				*self as u8
+			],
+		}
+	}
+}
+
+impl U8conversion for u32
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		let mut u8_vec: Vec<u8> = Vec::new();
+
+		for i in 0..4
+		{
+			u8_vec.push((*self >> (8 * (if *endian == Endian::Big {3-i} else {i} ) )) as u8);
+		}
+
+		return u8_vec;
+	}
 }
