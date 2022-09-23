20e16dfe31d1064c710d5a98b56f7057695b5404
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-23 02:55:49 +0200

    Update endian.rs
    Working on u8 conversion

--- src/endian.rs
+++ src/endian.rs
@@ -18,7 +18,60 @@ U8conversion
 	-> Vec<u8>;
 }
 
+
+impl U8conversion for u8
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		vec![*self]
+	}
+}
+
+impl U8conversion for i8
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		vec![(*self) as u8]
+	}
+}
+
 impl U8conversion for u16
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		match *endian
+		{
+			Endian::Little => vec![
+				*self as u8, 
+				(*self >> 8) as u8
+			],
+			Endian::Big => vec![
+				(*self >> 8) as u8,
+				*self as u8
+			],
+		}
+	}
+}
+
+impl U8conversion for i16
 {
 	fn
 	to_u8_vec
@@ -62,3 +115,20 @@ impl U8conversion for u32
 		return u8_vec;
 	}
 }
+
+impl U8conversion for String
+{
+	fn
+	to_u8_vec
+	(
+		&self,
+		endian: &Endian
+	)
+	-> Vec<u8>
+	{
+		let mut u8_vec = self.as_bytes().to_vec();
+		u8_vec.push(0x00 as u8);
+		return u8_vec;
+	}
+}
+
