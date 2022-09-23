f1468c0ad0fcbe119014daae4e0ec2bfd6dd1b17
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-23 18:13:44 +0200

    Create png_chunk.rs
    Moved PngChunk enum into own file

---
+++ src/png_chunk.rs
@@ -0,0 +1,135 @@
+pub enum
+PngChunkOrdering
+{
+	FIRST,
+	BEFORE_IDAT,
+	BEFORE_PLTE_AND_IDAT,
+	AFTER_PLTE_BEFORE_IDAT,
+	LAST,
+	NONE
+}
+
+macro_rules! build_png_chunk_type_enum {
+	(
+		$( (
+			$tag:ident,
+			$critical:expr,
+			$multiple:expr,
+			$ordering:ident
+		) ),*
+	)
+	=>
+	{
+		pub enum
+		PngChunk
+		{
+			$(
+				$tag(u32),
+			)*
+		}
+
+		impl PngChunk
+		{
+			pub fn
+			is_critical
+			(
+				&self
+			)
+			-> bool
+			{
+				match *self
+				{
+					$(
+						PngChunk::$tag(_) => $critical,
+					)*
+				}
+			}
+
+			pub fn
+			allows_multiple
+			(
+				&self
+			)
+			-> bool
+			{
+				match *self
+				{
+					$(
+						PngChunk::$tag(_) => $multiple,
+					)*
+				}
+			}
+
+			pub fn
+			ordering
+			(
+				&self
+			)
+			-> PngChunkOrdering
+			{
+				match *self
+				{
+					$(
+						PngChunk::$tag(_) => PngChunkOrdering::$ordering,
+					)*
+				}
+			}
+
+			pub fn
+			length
+			(
+				&self
+			)
+			-> u32
+			{
+				match *self
+				{
+					$(
+						PngChunk::$tag(length) => length,
+					)*
+				}
+			}
+
+			pub fn
+			as_string
+			(
+				&self
+			)
+			-> String
+			{
+				match *self
+				{
+					$(
+						PngChunk::$tag(_) => String::from(stringify!($tag)),
+					)*
+				}
+			}
+
+			pub fn
+			from_string
+			(
+				string_name: &String,
+				length: u32
+			)
+			-> Result<PngChunk, String>
+			{
+				match &(*string_name.as_str())
+				{
+					$(
+						stringify!($tag) => Ok(PngChunk::$tag(length)),
+					)*
+					_ => Err("Invalid chunk name".to_string()),
+				}
+			}
+		}
+	}
+}
+
+build_png_chunk_type_enum![
+	// Tag	Critical	Multiple	Ordering
+	(IHDR,	true,		false,		FIRST),
+	(PLTE,	true,		false,		BEFORE_IDAT),
+	(IDAT,	true,		true,		NONE),
+	(IEND,	true,		false,		LAST),
+	(zTXt,	false,		true,		NONE)
+];
