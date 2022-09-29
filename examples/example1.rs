47780408a23bfd468426122f7b8adcaf4099a31f
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-29 11:42:39 +0200

    Update example1.rs
    Extended the example to be used as a testing facility (for now)

--- examples/example1.rs
+++ examples/example1.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 extern crate little_exif;
 use little_exif::metadata::Metadata;
 use little_exif::exif_tag::ExifTag;
+use little_exif::exif_tag::ExifTagGroup;
 
 fn
 main()
@@ -18,15 +19,31 @@ main()
 	let mut data = Metadata::new();
 
 	// Set the ImageDescription (IFD0) an ISO (ExifIFD) tag as examples
+
+	data.set_tag(
+		ExifTag::UnknownSTRING("test".to_string(), 0x010d, ExifTagGroup::IFD0)
+	);
+
 	data.set_tag(
 		ExifTag::ImageDescription("-w 1000 -h 1000 --x_mid=0 --y_mid=0 -z 0.5 -i 1000 -c 8".to_string())
 	);
+
 	data.set_tag(
 		ExifTag::ISO(vec![2022])
 	);
 
+	data.set_tag(
+		ExifTag::UnknownSTRING("test".to_string(), 0x010c, ExifTagGroup::IFD0)
+	);
+
 	// Write the metadata to the copy
 	let write_result = data.write_to_file(Path::new("examples/copy.png"));
+
+	let encoded_data = data.encode_metadata_png();
+	let decoded_data1 = Metadata::decode_metadata_png(&encoded_data);
+	let decoded_data2 = Metadata::decode_metadata_general(&decoded_data1);
+
+	
 
 	if write_result.is_err()
 	{
