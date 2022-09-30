030c9b746ebf0f35d88a59aea6289595ddd57c89
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-01 00:21:56 +0200

    Update png.rs
    Moved PNG specific encoding/decoding to png.rs (will be removed from metadata.rs in future commit)

--- src/png.rs
+++ src/png.rs
@@ -5,6 +5,7 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::collections::VecDeque;
 
 use crc::{Crc, CRC_32_ISO_HDLC};
 use miniz_oxide::deflate::compress_to_vec_zlib;
@@ -20,6 +21,162 @@ pub const RAW_PROFILE_TYPE_EXIF: [u8; 23] = [
 	0x74, 0x79, 0x70, 0x65, 0x20,						// type
 	0x65, 0x78, 0x69, 0x66, 0x00, 0x00					// exif NUL NUL
 ];
+
+// The bytes during encoding need to be encoded themselves:
+// A given byte (e.g. 0x30 for the char '0') has two values in the string of its hex representation ('3' and '0')
+// These two characters need to be encoded themselves (51 for '3', 48 for '0'), resulting in the final encoded
+// version of the EXIF data
+// Independent of endian as this does not affect the ordering of values WITHIN a byte 
+fn encode_byte(byte: &u8) -> [u8; 2] 
+{
+	[
+		byte / 16 + (if byte / 16 < 10 {'0' as u8} else {'a' as u8 - 10}),
+		byte % 16 + (if byte % 16 < 10 {'0' as u8} else {'a' as u8 - 10}) 
+	]
+}
+
+fn
+encode_metadata_png
+(
+	exif_vec: &Vec<u8>
+)
+-> Vec<u8>
+{
+	// The size of the EXIF data area, consists of
+	// - length of EXIF header (follows after ssss)
+	// - length of exif_vec
+	// - 1 for ssss itself (why not 4? idk)
+	let ssss = (
+		EXIF_HEADER.len()	as u32 
+		+ exif_vec.len()	as u32 
+		+ 1					as u32
+	).to_string();
+
+	// Construct final vector with the bytes as they will be sent to the encoder
+	//                               \n       e     x     i     f     \n
+	let mut png_exif: Vec<u8> = vec![NEWLINE, 0x65, 0x78, 0x69, 0x66, NEWLINE];
+
+	// Write ssss
+	for _ in 0..(8-ssss.len())
+	{
+		png_exif.push(SPACE);
+	}
+	png_exif.extend(ssss.as_bytes().to_vec().iter());
+	png_exif.push(NEWLINE);
+
+	// Write EXIF header and previously constructed EXIF data as encoded bytes
+	for byte in &EXIF_HEADER
+	{
+		png_exif.extend(encode_byte(byte).iter());
+	}
+
+	for byte in exif_vec
+	{
+		png_exif.extend(encode_byte(byte).iter());
+	}
+	
+	// Write end of EXIF data - 2* 0x30 results in the String "00" for 0x00
+	png_exif.push(0x30);
+	png_exif.push(0x30);
+	png_exif.push(NEWLINE);
+
+	return png_exif;
+}
+
+fn
+decode_metadata_png
+(
+	encoded_data: &Vec<u8>
+)
+-> Result<Vec<u8>, std::io::Error>
+{
+
+	let mut exif_all: VecDeque<u8> = VecDeque::new();
+	let mut other_byte: Option<u8> = None;
+
+	// This performs the reverse operation to encode_byte:
+	// Two successing bytes represent the ASCII values of the digits of 
+	// a hex value, e.g. 0x31, 0x32 represent '1' and '2', so the resulting
+	// hex value is 0x12, which gets pushed onto exif_all
+	for byte in encoded_data
+	{
+		// Ignore newline characters
+		if *byte == '\n' as u8
+		{
+			continue;
+		}
+
+		if other_byte.is_none()
+		{
+			other_byte = Some(*byte);
+			continue;
+		}
+
+		let value_string = "".to_owned()
+			+ &(other_byte.unwrap() as char).to_string()
+			+ &(*byte as char).to_string();
+		if let Ok(value) = u8::from_str_radix(value_string.trim(), 16)
+		{
+			exif_all.push_back(value);
+		}
+		
+		other_byte = None;
+	}
+
+	// Now remove the first element until the exif header is found
+	// Store the popped elements to get the size information
+	let mut exif_header_found = false;
+	let mut pop_storage: Vec<u8> = Vec::new();
+
+	while !exif_header_found
+	{
+		let mut counter = 0;
+		for header_value in &EXIF_HEADER
+		{
+			if *header_value != exif_all[counter]
+			{
+				break;
+			}
+			counter += 1;
+		}
+
+		exif_header_found = counter == EXIF_HEADER.len();
+
+		if exif_header_found
+		{
+			break;
+		}
+		pop_storage.push(exif_all.pop_front().unwrap());
+	}
+
+	// The exif header has been found
+	// -> exif_all now starts with the exif header information
+	// -> pop_storage has in its last 4 elements the size information
+	//    that will now get extracted
+	// Consider this part optional as it might be removed in the future and
+	// isn't strictly necessary and just for validating the data we get
+	assert!(pop_storage.len() > 0);
+
+	// Using the encode_byte function re-encode the bytes regarding the size
+	// information and construct its value using decimal based shifting
+	// Example: 153 = 0
+	// + 5*10*10^(2*0) + 3*1*10^(2*0) 
+	// + 0*10*10^(2*1) + 1*1*10^(2*1)
+	let mut given_exif_len = 0u64;
+	for i in 0..std::cmp::min(4, pop_storage.len())
+	{
+		let re_encoded_byte = encode_byte(&pop_storage[pop_storage.len() -1 -i]);
+		let tens_place = u64::from_str_radix(&(re_encoded_byte[0] as char).to_string(), 10).unwrap();
+		let ones_place = u64::from_str_radix(&(re_encoded_byte[1] as char).to_string(), 10).unwrap();
+		given_exif_len = given_exif_len + tens_place * 10 * 10_u64.pow((2 * i).try_into().unwrap());
+		given_exif_len = given_exif_len + ones_place *  1 * 10_u64.pow((2 * i).try_into().unwrap());
+	}
+
+	assert!(given_exif_len == exif_all.len().try_into().unwrap());
+	// End optional part
+
+	return Ok(Vec::from(exif_all));
+}
 
 fn
 check_signature
@@ -244,15 +401,86 @@ clear_metadata
 	return Ok(());
 }
 
+pub fn
+read_metadata
+(
+	path: &Path
+)
+-> Result<Vec<u8>, std::io::Error>
+{
+	// Parse the PNG - if this fails, the read fails as well
+	let parse_png_result = parse_png(path);
+	if let Err(error) = parse_png_result
+	{
+		return Err(error);
+	}
+
+	// Parsed PNG is Ok to use - Open the file and go through the chunks
+	let mut file = check_signature(path).unwrap();
+	for chunk in &parse_png_result.unwrap()
+	{
+		// Wrong chunk? Seek to the next one
+		if chunk.as_string() != String::from("zTXt")
+		{
+			perform_file_action!(file.seek(SeekFrom::Current(chunk.length() as i64 + 12)));
+		}
+
+		// We now have a zTXt chunk:
+		// Skip chunk length and type (4+4 Bytes)
+		perform_file_action!(file.seek(SeekFrom::Current(8)));
+
+		// Read chunk data into buffer
+		// No need to verify this using CRC as already done by parse_png(path)
+		let mut zTXt_chunk_data = vec![0u8; chunk.length() as usize];
+		if file.read(&mut zTXt_chunk_data).unwrap() != chunk.length() as usize
+		{
+			return io_error!(Other, "Could not read chunk data");
+		}
+
+		// Check that this is the correct zTXt chunk...
+		let mut correct_zTXt_chunk = true;
+		for i in 0..RAW_PROFILE_TYPE_EXIF.len()
+		{
+			if zTXt_chunk_data[i] != RAW_PROFILE_TYPE_EXIF[i]
+			{
+				correct_zTXt_chunk = false;
+			}
+		}
+
+		if !correct_zTXt_chunk
+		{
+			// Skip CRC from current (wrong) zTXt chunk and continue
+			perform_file_action!(file.seek(SeekFrom::Current(4)));
+			continue;
+		}
+
+		// Decode zlib data...
+		if let Ok(decompressed_data) = decompress_to_vec_zlib(&zTXt_chunk_data[RAW_PROFILE_TYPE_EXIF.len()..])
+		{
+			// ...and perform PNG-specific decoding & return the result
+			return Ok(decode_metadata_png(&decompressed_data).unwrap());
+		}
+		else
+		{
+			return io_error!(Other, "Could not inflate compressed chunk data!");
+		}
+	}
+
+	return io_error!(Other, "No metadata found!");
+
+}
+
 #[allow(non_snake_case)]
 pub fn
 write_metadata
 (
 	path: &Path,
-	encoded_metadata: &Vec<u8>
+	general_encoded_metadata: &Vec<u8>
 )
 -> Result<(), std::io::Error>
 {
+
+	let encoded_metadata = encode_metadata_png(general_encoded_metadata);
 
 	// First clear the existing metadata
 	// This also parses the PNG and checks its validity, so it is safe to
@@ -311,66 +539,4 @@ write_metadata
 	perform_file_action!(file.write_all(&buffer));
 
 	return Ok(());
-}
-
-pub fn
-read_metadata
-(
-	path: &Path
-)
--> Result<Vec<u8>, std::io::Error>
-{
-	// Parse the PNG - if this fails, the read fails as well
-	let parse_png_result = parse_png(path);
-	if let Err(error) = parse_png_result
-	{
-		return Err(error);
-	}
-
-	// Parsed PNG is Ok to use - Open the file and go through the chunks
-	let mut file = check_signature(path).unwrap();
-	for chunk in &parse_png_result.unwrap()
-	{
-		// Wrong chunk? Seek to the next one
-		if chunk.as_string() != String::from("zTXt")
-		{
-			perform_file_action!(file.seek(SeekFrom::Current(chunk.length() as i64 + 12)));
-		}
-
-		// We now have a zTXt chunk:
-		// Skip chunk length and type (4+4 Bytes)
-		perform_file_action!(file.seek(SeekFrom::Current(8)));
-
-		// Read chunk data into buffer
-		// No need to verify this using CRC as already done by parse_png(path)
-		let mut zTXt_chunk_data = vec![0u8; chunk.length() as usize];
-		if file.read(&mut zTXt_chunk_data).unwrap() != chunk.length() as usize
-		{
-			return io_error!(Other, "Could not read chunk data");
-		}
-
-		// Check that this is the correct zTXt chunk...
-		let mut correct_zTXt_chunk = true;
-		for i in 0..RAW_PROFILE_TYPE_EXIF.len()
-		{
-			if zTXt_chunk_data[i] != RAW_PROFILE_TYPE_EXIF[i]
-			{
-				correct_zTXt_chunk = false;
-			}
-		}
-
-		if !correct_zTXt_chunk
-		{
-			// Skip CRC from current (wrong) zTXt chunk and continue
-			perform_file_action!(file.seek(SeekFrom::Current(4)));
-			continue;
-		}
-
-		// Decode zlib data and return
-		return Ok(decompress_to_vec_zlib(&zTXt_chunk_data[RAW_PROFILE_TYPE_EXIF.len()..]).unwrap());
-		
-	}
-
-	return io_error!(Other, "No metadata found!");
-
 }
