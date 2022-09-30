8a0845ddb6581ac5b578acb83c8491abaccc3d3f
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-01 00:21:09 +0200

    Update general_file_io.rs
    Moved some constants here, away from metadata.rs

--- src/general_file_io.rs
+++ src/general_file_io.rs
@@ -1,3 +1,7 @@
+pub const NEWLINE: u8 = 0x0a;
+pub const SPACE: u8 = 0x20;
+pub const EXIF_HEADER: [u8; 6] = [0x45, 0x78, 0x69, 0x66, 0x00, 0x00];
+
 macro_rules! perform_file_action {
 	( 
 		$action: expr
