ddc4630984ced17bf26bf8e81d99690e54ee716b
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-01 00:27:02 +0200

    Update metadata.rs
    Removal of file-type-specific functionality (see previous commits)

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use std::collections::VecDeque;
 
-use crate::endian::{Endian, U8conversion};
+use crate::endian::*;
 use crate::exif_tag::{ExifTag, ExifTagGroup};
 use crate::exif_tag_format::ExifTagFormat;
 use crate::general_file_io::*;
@@ -9,31 +9,8 @@ use crate::general_file_io::*;
 use crate::jpg;
 use crate::png;
 
-const NEWLINE: u8 = 0x0a;
-const SPACE: u8 = 0x20;
-
-const EXIF_HEADER: [u8; 6] = [0x45, 0x78, 0x69, 0x66, 0x00, 0x00];
-
-const JPG_APP1_MARKER: [u8; 2] = [0xff, 0xe1];
-
 const IFD_ENTRY_LENGTH: u32 = 12;
 const IFD_END: [u8; 4] = [0x00, 0x00, 0x00, 0x00];
-
-macro_rules! to_u8_vec_macro {
-	($type:ty, $value:expr, $endian:expr)
-	=>
-	{
-		<$type as U8conversion<$type>>::to_u8_vec($value, $endian)
-	};
-}
-
-macro_rules! from_u8_vec_macro {
-	($type:ty, $value:expr, $endian:expr)
-	=>
-	{
-		<$type as U8conversion<$type>>::from_u8_vec($value, $endian)
-	}
-}
 
 pub struct
 Metadata
@@ -82,7 +59,7 @@ Metadata
 		{
 			// "jpg"	=> jpg::read_metadata(&path),
 			// "jpeg"	=> jpg::read_metadata(&path, &self.encode_metadata_jpg()),
-			"png"	=> Self::decode_metadata_png(&png::read_metadata(&path).unwrap()),
+			"png"	=> png::read_metadata(&path),
 			_		=> panic!("Unsupported file type!"),
 		}
 		{
@@ -192,111 +169,15 @@ Metadata
 		
 		match file_type_str.unwrap().to_lowercase().as_str()
 		{
-			"jpg"	=> jpg::write_metadata(&path, &self.encode_metadata_jpg()),
-			"jpeg"	=> jpg::write_metadata(&path, &self.encode_metadata_jpg()),
-			"png"	=> png::write_metadata(&path, &self.encode_metadata_png()),
+			"jpg"	=> jpg::write_metadata(&path, &self.encode_metadata_general()),
+			"jpeg"	=> jpg::write_metadata(&path, &self.encode_metadata_general()),
+			"png"	=> png::write_metadata(&path, &self.encode_metadata_general()),
 			_		=> io_error!(Unsupported, "Unsupported file type!"),
 		}
 
 	}
 
-
-	pub fn
-	decode_metadata_png
-	(
-		encoded_data: &Vec<u8>
-	)
-	-> Result<Vec<u8>, std::io::Error>
-	{
-
-		let mut exif_all: VecDeque<u8> = VecDeque::new();
-		let mut other_byte: Option<u8> = None;
-
-		// This performs the reverse operation to encode_byte:
-		// Two successing bytes represent the ASCII values of the digits of 
-		// a hex value, e.g. 0x31, 0x32 represent '1' and '2', so the resulting
-		// hex value is 0x12, which gets pushed onto exif_all
-		for byte in encoded_data
-		{
-			// Ignore newline characters
-			if *byte == '\n' as u8
-			{
-				continue;
-			}
-
-			if other_byte.is_none()
-			{
-				other_byte = Some(*byte);
-				continue;
-			}
-
-			let value_string = "".to_owned()
-				+ &(other_byte.unwrap() as char).to_string()
-				+ &(*byte as char).to_string();
-			if let Ok(value) = u8::from_str_radix(value_string.trim(), 16)
-			{
-				exif_all.push_back(value);
-			}
-			
-			other_byte = None;
-		}
-
-		// Now remove the first element until the exif header is found
-		// Store the popped elements to get the size information
-		let mut exif_header_found = false;
-		let mut pop_storage: Vec<u8> = Vec::new();
-
-		while !exif_header_found
-		{
-			let mut counter = 0;
-			for header_value in &EXIF_HEADER
-			{
-				if *header_value != exif_all[counter]
-				{
-					break;
-				}
-				counter += 1;
-			}
-
-			exif_header_found = counter == EXIF_HEADER.len();
-
-			if exif_header_found
-			{
-				break;
-			}
-			pop_storage.push(exif_all.pop_front().unwrap());
-		}
-
-		// The exif header has been found
-		// -> exif_all now starts with the exif header information
-		// -> pop_storage has in its last 4 elements the size information
-		//    that will now get extracted
-		// Consider this part optional as it might be removed in the future and
-		// isn't strictly necessary and just for validating the data we get
-		assert!(pop_storage.len() > 0);
-
-		// Using the encode_byte function re-encode the bytes regarding the size
-		// information and construct its value using decimal based shifting
-		// Example: 153 = 0
-		// + 5*10*10^(2*0) + 3*1*10^(2*0) 
-		// + 0*10*10^(2*1) + 1*1*10^(2*1)
-		let mut given_exif_len = 0u64;
-		for i in 0..std::cmp::min(4, pop_storage.len())
-		{
-			let re_encoded_byte = Self::encode_byte(&pop_storage[pop_storage.len() -1 -i]);
-			let tens_place = u64::from_str_radix(&(re_encoded_byte[0] as char).to_string(), 10).unwrap();
-			let ones_place = u64::from_str_radix(&(re_encoded_byte[1] as char).to_string(), 10).unwrap();
-			given_exif_len = given_exif_len + tens_place * 10 * 10_u64.pow((2 * i).try_into().unwrap());
-			given_exif_len = given_exif_len + ones_place *  1 * 10_u64.pow((2 * i).try_into().unwrap());
-		}
-
-		assert!(given_exif_len == exif_all.len().try_into().unwrap());
-		// End optional part
-
-		return Ok(Vec::from(exif_all));
-	}
-
-	pub fn
+	fn
 	decode_metadata_general
 	(
 		encoded_data: &Vec<u8>
@@ -615,113 +496,6 @@ Metadata
 
 		// Other directories here... (someday)
 		
-		/*
-		let mut counter = 0u32;
-		for byte in &exif_vec
-		{
-			if *byte > 0x20
-			{
-				println!("{:#04x} {:#04x} {}", counter, *byte, *byte as char);
-			}
-			else
-			{
-				println!("{:#04x} {:#04x}", counter, *byte);
-			}
-			
-			counter += 1;
-		}
-		*/
-		
 		return exif_vec;
-	}
-
-	// The bytes during encoding need to be encoded themselves:
-	// A given byte (e.g. 0x30 for the char '0') has two values in the string of its hex representation ('3' and '0')
-	// These two characters need to be encoded themselves (51 for '3', 48 for '0'), resulting in the final encoded
-	// version of the EXIF data
-	// Independent of endian as this does not affect the ordering of values WITHIN a byte 
-	fn encode_byte(byte: &u8) -> [u8; 2] 
-	{
-		[
-			byte / 16 + (if byte / 16 < 10 {'0' as u8} else {'a' as u8 - 10}),
-			byte % 16 + (if byte % 16 < 10 {'0' as u8} else {'a' as u8 - 10}) 
-		]
-	}
-
-	pub fn
-	encode_metadata_png
-	(
-		&self
-	)
-	-> Vec<u8>
-	{
-		// Get the general version of the encoded metadata
-		let exif_vec = self.encode_metadata_general();
-
-		// The size of the EXIF data area, consists of
-		// - length of EXIF header (follows after ssss)
-		// - length of exif_vec
-		// - 1 for ssss itself (why not 4? idk)
-		let ssss = (
-			EXIF_HEADER.len()	as u32 
-			+ exif_vec.len()	as u32 
-			+ 1					as u32
-		).to_string();
-
-		// Construct final vector with the bytes as they will be sent to the encoder
-		//                               \n       e     x     i     f     \n
-		let mut png_exif: Vec<u8> = vec![NEWLINE, 0x65, 0x78, 0x69, 0x66, NEWLINE];
-
-		// Write ssss
-		for _ in 0..(8-ssss.len())
-		{
-			png_exif.push(SPACE);
-		}
-		png_exif.extend(ssss.as_bytes().to_vec().iter());
-		png_exif.push(NEWLINE);
-
-		// Write EXIF header and previously constructed EXIF data as encoded bytes
-		for byte in &EXIF_HEADER
-		{
-			png_exif.extend(Self::encode_byte(byte).iter());
-		}
-
-		for byte in &exif_vec
-		{
-			png_exif.extend(Self::encode_byte(byte).iter());
-		}
-		
-		// Write end of EXIF data - 2* 0x30 results in the String "00" for 0x00
-		png_exif.push(0x30);
-		png_exif.push(0x30);
-		png_exif.push(NEWLINE);
-
-		return png_exif;
-	}
-
-	fn
-	encode_metadata_jpg
-	(
-		&self
-	)
-	-> Vec<u8>
-	{
-		// Get the general version of the encoded metadata
-		let exif_vec = self.encode_metadata_general();
-
-		// vector storing the data that will be returned
-		let mut jpg_exif: Vec<u8> = Vec::new();
-
-		// Compute the length of the exif data (includes the two bytes of the
-		// actual length field)
-		let length = 2u16 + (exif_vec.len() as u16);
-
-		// Start with the APP1 marker and the length of the data
-		// Then copy the previously encoded EXIF data 
-		jpg_exif.extend(JPG_APP1_MARKER.iter());
-		jpg_exif.extend(to_u8_vec_macro!(u16, &length, &Endian::Big));
-		jpg_exif.extend(exif_vec.iter());
-
-		return jpg_exif;
 	}
 }
