5daa80bce7c478d11206ca7f0c793c851faca9ae
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-01 11:47:01 +0200

    Update jpg.rs
    Working on parsing JPG data and clearing metadata - however, there seems to be a problem with parsing...

--- src/jpg.rs
+++ src/jpg.rs
@@ -9,9 +9,19 @@ use std::fs::OpenOptions;
 use crc::{Crc, CRC_32_ISO_HDLC};
 
 use crate::endian::*;
+use crate::general_file_io::*;
 
 pub const JPG_SIGNATURE: [u8; 2] = [0xff, 0xd8];
-const JPG_APP1_MARKER: [u8; 2] = [0xff, 0xe1];
+// const JPG_APP1_MARKER: [u8; 2] = [0xff, 0xe1];
+const JPG_APP1_MARKER: u16 = 0xffe1;
+
+struct
+JPGsegment
+{
+	marker: u16,
+	length: u16,
+	data: Vec<u8>
+}
 
 fn
 encode_metadata_jpg
@@ -29,7 +39,7 @@ encode_metadata_jpg
 
 	// Start with the APP1 marker and the length of the data
 	// Then copy the previously encoded EXIF data 
-	jpg_exif.extend(JPG_APP1_MARKER.iter());
+	jpg_exif.extend(to_u8_vec_macro!(u16, &JPG_APP1_MARKER, &Endian::Big));
 	jpg_exif.extend(to_u8_vec_macro!(u16, &length, &Endian::Big));
 	jpg_exif.extend(exif_vec.iter());
 
@@ -41,11 +51,11 @@ check_signature
 (
 	path: &Path
 )
--> Result<File, String>
+-> Result<File, std::io::Error>
 {
 	if !path.exists()
 	{
-		return Err("Can't open JPG file - File does not exist!".to_string());
+		return io_error!(NotFound, "Can't parse JPG file - File does not exist!");
 	}
 
 	let mut file = OpenOptions::new()
@@ -63,11 +73,56 @@ check_signature
 
 	if !signature_is_valid
 	{
-		return Err("Can't open JPG file - Wrong signature!".to_string());
+		return io_error!(InvalidData, "Can't open JPG file - Wrong signature!");
 	}
 
 	// Signature is valid - can proceed using the file as JPG file
 	return Ok(file);
+}
+
+fn
+parse_jpg
+(
+	path: &Path
+)
+-> Result<Vec<JPGsegment>, std::io::Error>
+{
+	let mut file_result = check_signature(path);
+	let mut segments = Vec::new();
+
+	if file_result.is_err()
+	{
+		return Err(file_result.err().unwrap());
+	}
+
+	let mut file = file_result.unwrap();
+
+	loop
+	{
+		// First read the next marker
+		let mut marker_buffer = [0u8; 2];
+		perform_file_action!(file.read(&mut marker_buffer));
+
+		// Then read the length of the segment
+		let mut length_buffer = [0u8; 2];
+		perform_file_action!(file.read(&mut length_buffer));
+
+		// Decode the length to determine how much more data needs to be read
+		// For now I don't know how to determine endianness of a JPG
+		// But it seems like that (most) JPGs use big endian notation
+		let length = from_u8_vec_macro!(u16, &length_buffer.to_vec(), &Endian::Big);
+		let marker = from_u8_vec_macro!(u16, &marker_buffer.to_vec(), &Endian::Big);
+
+		// Read remaining data
+		// Note: The length includes the 2 bytes for the length field as well
+		println!("{}", length);
+		let mut data = vec![0u8; (length - 2) as usize];
+		perform_file_action!(file.read(&mut data));
+
+		segments.push(JPGsegment { marker, length, data } );
+	}
+
+	return Ok(segments);
 }
 
 pub fn
@@ -77,7 +132,44 @@ clear_metadata
 )
 -> Result<(), std::io::Error>
 {
-	Ok(())
+
+	let parse_jpg_result = parse_jpg(path);
+	if let Err(error) = parse_jpg_result
+	{
+		return Err(error);
+	}
+
+	let mut file = check_signature(path).unwrap();
+	let mut seek_counter = 2u64;
+
+	for segment in &parse_jpg_result.unwrap()
+	{
+
+		let total_seg_length = segment.length + 2;
+
+		if segment.marker != JPG_APP1_MARKER
+		{
+			seek_counter += total_seg_length as u64;
+			perform_file_action!(file.seek(SeekFrom::Current(total_seg_length as i64)));
+			continue;
+		}
+
+		// Get to the next section
+		perform_file_action!(file.seek(SeekFrom::Current(total_seg_length as i64)));
+
+		// ...copy data from there onwards into a buffer...
+		let mut buffer = Vec::new();
+		perform_file_action!(file.read_to_end(&mut buffer));
+
+		// ...go back to the chunk to be removed...
+		perform_file_action!(file.seek(SeekFrom::Start(seek_counter)));
+
+		// ...and overwrite it using the data from the buffer
+		perform_file_action!(file.write_all(&buffer));
+		perform_file_action!(file.seek(SeekFrom::Start(seek_counter)));		
+	}
+	
+	return Ok(());
 }
 
 pub fn
@@ -88,7 +180,12 @@ write_metadata
 )
 -> Result<(), std::io::Error>
 {
-	Ok(())
+	if let Err(error) = clear_metadata(path)
+	{
+		return Err(error);
+	}
+
+	return Ok(());
 }
 
 pub fn
