ea68d0c3462a9fe1945081ea54f9a308ab9e1157
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-01 14:51:05 +0200

    Rework of metadata clearing for JPGs
    Instead of parsing it first, go through the file byte for byte instead.

--- src/jpg.rs
+++ src/jpg.rs
@@ -12,16 +12,9 @@ use crate::endian::*;
 use crate::general_file_io::*;
 
 pub const JPG_SIGNATURE: [u8; 2] = [0xff, 0xd8];
-// const JPG_APP1_MARKER: [u8; 2] = [0xff, 0xe1];
-const JPG_APP1_MARKER: u16 = 0xffe1;
 
-struct
-JPGsegment
-{
-	marker: u16,
-	length: u16,
-	data: Vec<u8>
-}
+const JPG_MARKER_PREFIX: u8 = 0xff;
+const JPG_APP1_MARKER: u16 = 0xffe1;
 
 fn
 encode_metadata_jpg
@@ -55,11 +48,12 @@ check_signature
 {
 	if !path.exists()
 	{
-		return io_error!(NotFound, "Can't parse JPG file - File does not exist!");
+		return io_error!(NotFound, "Can't open JPG file - File does not exist!");
 	}
 
 	let mut file = OpenOptions::new()
 		.read(true)
+		.write(true)
 		.open(path)
 		.expect("Could not open file");
 	
@@ -80,96 +74,90 @@ check_signature
 	return Ok(file);
 }
 
-fn
-parse_jpg
+pub fn
+clear_metadata
 (
 	path: &Path
 )
--> Result<Vec<JPGsegment>, std::io::Error>
+-> Result<u8, std::io::Error>
 {
 	let mut file_result = check_signature(path);
-	let mut segments = Vec::new();
 
 	if file_result.is_err()
 	{
 		return Err(file_result.err().unwrap());
 	}
 
-	let mut file = file_result.unwrap();
+	// Setup of variables necessary for going through the file
+	let mut file = file_result.unwrap();										// The struct for interacting with the file
+	let mut seek_counter = 2u64;												// A counter for keeping track of where in the file we currently are
+	let mut byte_buffer = [0u8; 1];												// A buffer for reading in a byte of data from the file
+	let mut previous_byte_was_marker_prefix = false;							// A boolean for remembering if the previous byte was a marker prefix (0xFF)
+	let mut cleared_segments: u8 = 0;											// A counter for keeping track of how many segements were cleared
 
 	loop
 	{
-		// First read the next marker
-		let mut marker_buffer = [0u8; 2];
-		perform_file_action!(file.read(&mut marker_buffer));
+		// Read next byte into buffer
+		perform_file_action!(file.read(&mut byte_buffer));
+		println!("{} {:#04x}", seek_counter, byte_buffer[0]);
 
-		// Then read the length of the segment
-		let mut length_buffer = [0u8; 2];
-		perform_file_action!(file.read(&mut length_buffer));
-
-		// Decode the length to determine how much more data needs to be read
-		// For now I don't know how to determine endianness of a JPG
-		// But it seems like that (most) JPGs use big endian notation
-		let length = from_u8_vec_macro!(u16, &length_buffer.to_vec(), &Endian::Big);
-		let marker = from_u8_vec_macro!(u16, &marker_buffer.to_vec(), &Endian::Big);
-
-		// Read remaining data
-		// Note: The length includes the 2 bytes for the length field as well
-		println!("{}", length);
-		let mut data = vec![0u8; (length - 2) as usize];
-		perform_file_action!(file.read(&mut data));
-
-		segments.push(JPGsegment { marker, length, data } );
-	}
-
-	return Ok(segments);
-}
-
-pub fn
-clear_metadata
-(
-	path: &Path
-)
--> Result<(), std::io::Error>
-{
-
-	let parse_jpg_result = parse_jpg(path);
-	if let Err(error) = parse_jpg_result
-	{
-		return Err(error);
-	}
-
-	let mut file = check_signature(path).unwrap();
-	let mut seek_counter = 2u64;
-
-	for segment in &parse_jpg_result.unwrap()
-	{
-
-		let total_seg_length = segment.length + 2;
-
-		if segment.marker != JPG_APP1_MARKER
+		if previous_byte_was_marker_prefix
 		{
-			seek_counter += total_seg_length as u64;
-			perform_file_action!(file.seek(SeekFrom::Current(total_seg_length as i64)));
-			continue;
+			match byte_buffer[0]
+			{
+				0xe1	=> {													// APP1 marker
+
+					// Read in the length of the segment
+					// (which follows immediately after the marker)
+					let mut length_buffer = [0u8; 2];
+					perform_file_action!(file.read(&mut length_buffer));
+
+					// Decode the length to determine how much more data there is
+					let length = from_u8_vec_macro!(u16, &length_buffer.to_vec(), &Endian::Big);
+					let remaining_length = length - 2;
+
+					// Get to the next section
+					perform_file_action!(file.seek(SeekFrom::Current(remaining_length as i64)));
+
+					// ...copy data from there onwards into a buffer...
+					let mut buffer = Vec::new();
+					perform_file_action!(file.read_to_end(&mut buffer));
+
+					// ...go back to the chunk to be removed...
+					// Note on why -1: This has to do with "previous_byte_was_marker_prefix"
+					// We need to overwrite this byte as well - however, it was 
+					// read in the *previous* iteration, not this one
+					perform_file_action!(file.seek(SeekFrom::Start(seek_counter-1)));
+
+					// ...and overwrite it using the data from the buffer
+					perform_file_action!(file.write_all(&buffer));
+
+					// Seek back to where we started and decrement the seek_counter
+					// by 2 (= length of marker) as it will be incremented at
+					// the end of the loop again
+					perform_file_action!(file.seek(SeekFrom::Start(seek_counter-1)));
+
+					seek_counter -= 2;
+					cleared_segments += 1;
+				},
+				0xd9	=> {													// EOI marker
+					return Ok(cleared_segments);
+				}
+				_		=> (),													// Every other marker
+			}
+
+			previous_byte_was_marker_prefix = false;
+		}
+		else
+		{
+			previous_byte_was_marker_prefix = byte_buffer[0] == JPG_MARKER_PREFIX;
 		}
 
-		// Get to the next section
-		perform_file_action!(file.seek(SeekFrom::Current(total_seg_length as i64)));
+		seek_counter += 1;
 
-		// ...copy data from there onwards into a buffer...
-		let mut buffer = Vec::new();
-		perform_file_action!(file.read_to_end(&mut buffer));
-
-		// ...go back to the chunk to be removed...
-		perform_file_action!(file.seek(SeekFrom::Start(seek_counter)));
-
-		// ...and overwrite it using the data from the buffer
-		perform_file_action!(file.write_all(&buffer));
-		perform_file_action!(file.seek(SeekFrom::Start(seek_counter)));		
 	}
-	
-	return Ok(());
+
+	return Ok(cleared_segments);
 }
 
 pub fn
@@ -180,10 +168,17 @@ write_metadata
 )
 -> Result<(), std::io::Error>
 {
-	if let Err(error) = clear_metadata(path)
+	let clearing_result = clear_metadata(path);
+	if let Err(error) = clearing_result
 	{
+		println!("{}", error);
 		return Err(error);
 	}
+	else
+	{
+		println!("Cleared {} segments!", clearing_result.unwrap());
+	}
+	
 
 	return Ok(());
 }
