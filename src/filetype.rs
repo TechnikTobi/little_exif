8a6316cfc6ac953b70a409f8f32dcb1f094f785d
Tobias Prisching <tobias.prisching@icloud.com>
2024-10-08 14:42:41 +0200

    Added tiff as filetype

--- src/filetype.rs
+++ src/filetype.rs
@@ -14,6 +14,7 @@ FileExtension
 	PNG  {as_zTXt_chunk: bool},
 	JPEG,
 	JXL,
+	TIFF,
 	WEBP
 }
 
@@ -37,6 +38,8 @@ FileExtension
 			"jpeg"  => Ok(FileExtension::JPEG),
 			"jxl"   => Ok(FileExtension::JXL),
 			"png"   => Ok(FileExtension::PNG{ as_zTXt_chunk: true}),
+			"tif"   => Ok(FileExtension::TIFF),
+			"tiff"  => Ok(FileExtension::TIFF),
 			"webp"  => Ok(FileExtension::WEBP),
 			_       => io_error!(Unsupported, "Unknown file type!")
 		}
