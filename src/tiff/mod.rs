7e34ca08c4e635a8c322c557271a62289b7f2f3e
Tobias Prisching <tobias.prisching@icloud.com>
2024-10-08 15:44:49 +0200

    Started work on TIFF

---
+++ src/tiff/mod.rs
@@ -0,0 +1,4 @@
+// Copyright © 2024 Tobias J. Prisching <tobias.prisching@icloud.com> and CONTRIBUTORS
+// See https://github.com/TechnikTobi/little_exif#license for licensing details
+
+pub mod file;
