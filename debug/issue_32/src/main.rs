e555dd1c757660e6cd2d326a36035468ac09a36e
Tobias Prisching <tobias.prisching@icloud.com>
2024-10-24 14:46:57 +0200

    Working on issue #32

--- debug/issue_32/src/main.rs
+++ debug/issue_32/src/main.rs
@@ -1,14 +1,18 @@
+use std::fs;
 use std::fs::copy;
 use std::fs::remove_file;
+use std::str::FromStr;
 use std::path::Path;
 
 extern crate little_exif;
 use little_exif::metadata::Metadata;
 use little_exif::exif_tag::ExifTag;
+use little_exif::filetype::get_file_type;
 
 fn main()
 -> Result<(), std::io::Error>
 {
+	/*
 	let orig_path = Path::new("./rsrc/ds218351.jpg");
 
 	// Read metadata from file
@@ -35,6 +39,18 @@ fn main()
 	{
 		println!("{:?}", tag);
 	}
+	*/
+
+	let source_file_path = "./rsrc/ds218351.jpg";
+
+	let mut file_content = fs::read(&source_file_path)?; // .context(source_token.source.clone())?;
+	let file_extension = get_file_type(Path::new(&source_file_path))?;
+	let mut metadata = Metadata::new_from_vec(&file_content, file_extension)?;
+
+	metadata.set_tag(ExifTag::ImageDescription("test".to_string()));
+	metadata.write_to_vec(&mut file_content, file_extension)?;
+
+	fs::write("./rsrc/copy.jpg", file_content)?;
 
 	Ok(())
 }
