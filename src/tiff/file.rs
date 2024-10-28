a677b6ca1756e4a524b3f499a9ddcd5daf7cbfa3
Tobias Prisching <tobias.prisching@icloud.com>
2024-10-28 12:11:55 +0100

    Working on TIFF support

--- src/tiff/file.rs
+++ src/tiff/file.rs
@@ -1,11 +1,16 @@
 // Copyright © 2024 Tobias J. Prisching <tobias.prisching@icloud.com> and CONTRIBUTORS
 // See https://github.com/TechnikTobi/little_exif#license for licensing details
 
-use std::io::Read;
+use std::io::BufReader;
+use std::io::BufWriter;
 use std::path::Path;
 
 use crate::general_file_io::open_read_file;
-use crate::general_file_io::EXIF_HEADER;
+use crate::general_file_io::open_write_file;
+use crate::metadata::Metadata;
+
+use super::generic_read_metadata;
+use super::generic_write_metadata;
 
 pub(crate) fn
 read_metadata
@@ -14,12 +19,35 @@ read_metadata
 )
 -> Result<Vec<u8>, std::io::Error>
 {
-	let mut file = open_read_file(path)?;
-	let mut buffer = Vec::new();
-	file.read_to_end(&mut buffer)?;
-	let mut tiff_with_exif_header = Vec::new();
-	tiff_with_exif_header.extend(EXIF_HEADER);
-	tiff_with_exif_header.append(&mut buffer);
+	let mut buffered_file = BufReader::new(open_read_file(path)?);
+	return generic_read_metadata(&mut buffered_file);
+}
 
-	return Ok(tiff_with_exif_header);
+pub(crate) fn
+clear_metadata
+(
+	path: &Path
+)
+-> Result<(), std::io::Error>
+{
+	// Read in the data
+	let     raw_data = generic_read_metadata(&mut BufReader::new(open_read_file(path)?));
+	let mut data     = Metadata::general_decoding_wrapper(raw_data)?;
+
+	// Remove all IFDs that aren't required
+	data.reduce_to_a_minimum();
+
+	// Write the reduced data back to the backup cursor
+	generic_write_metadata(&mut BufWriter::new(open_write_file(path)?), &data)?;
+
+	return Ok(());
+}
+
+pub(crate) fn 
+write_metadata
+(
+	
+) 
+{
+	
 }
