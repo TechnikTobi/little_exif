02e0ef1f416c1913644d71e056ebd165af9b77e8
Tobias Prisching <tobias.prisching@icloud.com>
2024-11-05 10:57:27 +0100

    Moving format conversion into own function

--- src/ifd/mod.rs
+++ src/ifd/mod.rs
@@ -287,77 +287,14 @@ ImageFileDirectory
 
 			// At this point we check if the format is actually what we expect
 			// it to be and convert it if possible
-			if tag.format().as_u16() != format.as_u16()
-			{
-				// The expected format and the given format in the file
-				// do *not* match. Check special cases (INT16U -> INT32U)
-				// If no special cases match, return an error
-				if 
-					tag.format() == ExifTagFormat::INT32U &&
-					format       == ExifTagFormat::INT16U
-				{
-					let int16u_data = <INT16U as U8conversion<INT16U>>::from_u8_vec(&raw_data, endian);
-					let int32u_data = int16u_data.into_iter().map(|x| x as u32).collect::<Vec<u32>>();
-
-					tag = tag.set_value_to_int32u_vec(int32u_data).unwrap();
-				}
-				else if
-					tag.format() == ExifTagFormat::INT16U &&
-					format       == ExifTagFormat::INT32U
-				{
-					let int32u_data = <INT32U as U8conversion<INT32U>>::from_u8_vec(&raw_data, endian);
-					let int16u_data = int32u_data.into_iter().map(|x| x as u16).collect::<Vec<u16>>();
-
-					tag = tag.set_value_to_int16u_vec(int16u_data).unwrap();
-				}
-				else if
-					tag.format()    == ExifTagFormat::INT8U  &&
-					format          == ExifTagFormat::STRING &&
-					tag.as_u16()    == 0x0005                && // GPSAltitudeRef
-					tag.get_group() == ExifTagGroup::GPS
-				{
-					// The GPSAltitudeRef tag is a strange case. It is the only
-					// GPS -Ref tag that is a INT8U, all others are STRINGs
-					// with a length of two. 
-					// Some images store this as a string nevertheless. 
-					// So, we try to convert the string by taking its first
-					// character. If it is 0x00 or 0x30 ("0") we set it to 0,
-					// if it is 0x01 or 0x31 ("1") we set it to 1, and
-					// otherwise we panic and tell the user to open a ticket.
-
-					let first_char = raw_data[0];
-					let int8u_data = match first_char
-					{
-						0x00 | 0x30 => vec![0u8],
-						0x01 | 0x31 => vec![1u8],
-						_ => panic!("Problem while decoding GPSAltitudeRef. Please open a new issue for little_exif!")
-					};
-
-					tag = ExifTag::from_u16_with_data(
-						0x0005, 
-						&ExifTagFormat::INT8U, 
-						&int8u_data, 
-						&endian, 
-						group
-					).unwrap();
-				}
-				// Other special cases
-				else
-				{
-					return io_error!(Other, format!("Illegal format for known tag! Tag: {:?} Expected: {:?} Got: {:?}", tag, tag.format(), format));
-				}
-			}
-			else
-			{
-				// Format is as expected; set the data by replacing the tag
-				tag = ExifTag::from_u16_with_data(
-					hex_tag, 
-					&format, 
-					&raw_data, 
-					&endian, 
-					group
-				).unwrap();
-			}
+			tag = Self::decode_tag_with_format_exceptions(
+				&tag,
+				format,
+				&raw_data,
+				endian,
+				hex_tag,
+				group
+			)?;
 
 			// Now we have at least confirmed that the format is ok (or has
 			// been corrected). Next, we need to differ between the two other
@@ -511,6 +448,91 @@ ImageFileDirectory
 			return Ok(None);
 		}
 		return Ok(Some(from_u8_vec_macro!(u32, &next_ifd_link_buffer, endian)));
+	}
+
+	fn
+	decode_tag_with_format_exceptions
+	(
+		raw_tag:  &ExifTag,
+		format:    ExifTagFormat,
+		raw_data: &Vec<u8>,
+		endian:   &Endian,
+		hex_tag:   u16,
+		group:    &ExifTagGroup
+	)
+	-> Result<ExifTag, std::io::Error>
+	{
+		if raw_tag.format().as_u16() != format.as_u16()
+		{
+			// The expected format and the given format in the file
+			// do *not* match. Check special cases (INT16U -> INT32U)
+			// If no special cases match, return an error
+			if 
+				raw_tag.format() == ExifTagFormat::INT32U &&
+				format           == ExifTagFormat::INT16U
+			{
+				let int16u_data = <INT16U as U8conversion<INT16U>>::from_u8_vec(raw_data, endian);
+				let int32u_data = int16u_data.into_iter().map(|x| x as u32).collect::<Vec<u32>>();
+
+				return Ok(raw_tag.set_value_to_int32u_vec(int32u_data).unwrap());
+			}
+			else if
+				raw_tag.format() == ExifTagFormat::INT16U &&
+				format           == ExifTagFormat::INT32U
+			{
+				let int32u_data = <INT32U as U8conversion<INT32U>>::from_u8_vec(raw_data, endian);
+				let int16u_data = int32u_data.into_iter().map(|x| x as u16).collect::<Vec<u16>>();
+
+				return Ok(raw_tag.set_value_to_int16u_vec(int16u_data).unwrap());
+			}
+			else if
+				raw_tag.format()    == ExifTagFormat::INT8U  &&
+				format              == ExifTagFormat::STRING &&
+				raw_tag.as_u16()    == 0x0005                && // GPSAltitudeRef
+				raw_tag.get_group() == ExifTagGroup::GPS
+			{
+				// The GPSAltitudeRef tag is a strange case. It is the only
+				// GPS -Ref tag that is a INT8U, all others are STRINGs
+				// with a length of two. 
+				// Some images store this as a string nevertheless. 
+				// So, we try to convert the string by taking its first
+				// character. If it is 0x00 or 0x30 ("0") we set it to 0,
+				// if it is 0x01 or 0x31 ("1") we set it to 1, and
+				// otherwise we panic and tell the user to open a ticket.
+
+				let first_char = raw_data[0];
+				let int8u_data = match first_char
+				{
+					0x00 | 0x30 => vec![0u8],
+					0x01 | 0x31 => vec![1u8],
+					_ => panic!("Problem while decoding GPSAltitudeRef. Please open a new issue for little_exif!")
+				};
+
+				return Ok(ExifTag::from_u16_with_data(
+					0x0005, 
+					&ExifTagFormat::INT8U, 
+					&int8u_data, 
+					&endian, 
+					group
+				).unwrap());
+			}
+			// Other special cases
+			else
+			{
+				return io_error!(Other, format!("Illegal format for known tag! Tag: {:?} Expected: {:?} Got: {:?}", raw_tag, raw_tag.format(), format));
+			}
+		}
+		else
+		{
+			// Format is as expected; set the data by replacing the tag
+			return Ok(ExifTag::from_u16_with_data(
+				hex_tag, 
+				&format, 
+				&raw_data, 
+				&endian, 
+				group
+			).unwrap());
+		}
 	}
 
 	/// Recursively encodes IFDs
