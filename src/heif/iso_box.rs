e501ad98e07672b0879153926d62ea022b4de56f
Tobias Prisching <tobias.prisching@icloud.com>
2025-06-04 16:37:08 +0200

    update

--- src/heif/iso_box.rs
+++ src/heif/iso_box.rs
@@ -10,17 +10,40 @@ BoxType
 }
 
 pub struct
-BaseBoxHeader
+BoxHeader
 {
     box_size:    usize,
     box_type:    BoxType,
     header_size: usize,
+    version:     Option<u8>,
+    flags:       Option<u32>,
 }
 
 pub struct
-ExtendedBoxHeader
+IsoBox
 {
-    base:    BaseBoxHeader,
-    version: u8,
-    flags:   u32,
+    header:    BoxHeader,
+    sub_boxes: Option<Vec<IsoBox>>,
+    data:      Vec<u8>,
+}
+
+// Examples:
+// - infe
+// 00000015: size of 0x15 bytes (including the 0x04 bytes of the size field itself) 
+// 696E6665: byte representation of `infe` 
+// 02:       version 2
+// 00000100 190000 
+// 6876633100: null terminated string of "hvc1"
+
+impl
+IsoBox
+{
+    pub(super) fn
+    parse
+    (
+        data: &[u8]
+    )
+    {
+
+    }
 }
