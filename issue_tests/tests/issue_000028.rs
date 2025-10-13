76faeee821307dd83460d0fcc4d4fa82176049cb
Tobias Prisching <tobias.prisching@icloud.com>
2025-10-13 11:33:41 +0200

    More issue based tests
    No tests for issues 32 and 34 as they are relying on third party software and we are unable to test something like this in a unit test

---
+++ issue_tests/tests/issue_000028.rs
@@ -0,0 +1,184 @@
+// Copyright © 2025 Tobias J. Prisching <tobias.prisching@icloud.com> and CONTRIBUTORS
+// including those who posted code snippets and/or image files for debugging
+// and testing purposes to the respective issue on GitHub.
+// See https://github.com/TechnikTobi/little_exif#license for licensing details
+
+/*
+Original problem:
+Within the 1500 jpg files under test several show library messages:
+
+Could not validate EXIF header!
+
+Error during decoding: Custom { kind: Other, error: "No EXIF data found!" }
+
+Could not get IFD0 tags:
+Could not decode SubIFD:
+Illegal format for known tag! Tag: FocalLengthIn35mmFormat([]) Expected: INT16U Got: INT32U
+
+Error during decoding: Error { kind: UnexpectedEof, message: "failed to fill whole buffer" }
+*/
+
+/*
+Solved:
+Added conversion function in special cases where two different types are 
+"acceptable" for the same tag, e.g. INT16U & INT32U (because camera makers are
+too incompetent to stick to the specifications)
+- e2951416652f613aa01a102f7b607d6db965a536
+*/
+
+use std::path::Path;
+
+extern crate little_exif_0_5_1;
+extern crate little_exif_0_6_0_beta_2;
+extern crate little_exif;
+
+#[test]
+#[should_panic (expected = "assertion `left == right` failed\n  left: 0\n right: 46")]
+fn
+read_exif_data_1_failed_version()
+{
+    let path1 = Path::new("resources/issue_000028/2017_aachen_abendhimmel_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in little_exif_0_5_1::metadata::Metadata::new_from_path(path1).unwrap().data()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 46);
+}
+
+#[test]
+fn
+read_exif_data_2_failed_version()
+{
+    let path2 = Path::new("resources/issue_000028/2017_isernhagen_sorento_1.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in little_exif_0_5_1::metadata::Metadata::new_from_path(path2).unwrap().data()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
+
+#[test]
+fn
+read_exif_data_3_failed_version()
+{
+    let path3 = Path::new("resources/issue_000028/2017_stockholm_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in little_exif_0_5_1::metadata::Metadata::new_from_path(path3).unwrap().data()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
+
+
+
+#[test]
+fn
+read_exif_data_1_fixed_version()
+{
+    let path1 = Path::new("resources/issue_000028/2017_aachen_abendhimmel_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif_0_6_0_beta_1::metadata::Metadata::new_from_path(path1).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 46);
+}
+
+#[test]
+fn
+read_exif_data_2_fixed_version()
+{
+    let path2 = Path::new("resources/issue_000028/2017_isernhagen_sorento_1.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif_0_6_0_beta_1::metadata::Metadata::new_from_path(path2).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
+
+#[test]
+fn
+read_exif_data_3_fixed_version()
+{
+    let path3 = Path::new("resources/issue_000028/2017_stockholm_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif_0_6_0_beta_1::metadata::Metadata::new_from_path(path3).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
+
+
+
+#[test]
+fn
+read_exif_data_1_current_version()
+{
+    let path1 = Path::new("resources/issue_000028/2017_aachen_abendhimmel_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif::metadata::Metadata::new_from_path(path1).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 46);
+}
+
+#[test]
+#[should_panic (expected = "called `Result::unwrap()` on an `Err` value: Custom { kind: Other, error: \"No EXIF data found!\" }")]
+fn
+read_exif_data_2_current_version()
+{
+    let path2 = Path::new("resources/issue_000028/2017_isernhagen_sorento_1.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif::metadata::Metadata::new_from_path(path2).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
+
+#[test]
+#[should_panic (expected = "called `Result::unwrap()` on an `Err` value: Custom { kind: Other, error: \"No EXIF data found!\" }")]
+fn
+read_exif_data_3_current_version()
+{
+    let path3 = Path::new("resources/issue_000028/2017_stockholm_emilio.jpg");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif::metadata::Metadata::new_from_path(path3).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 0);
+}
---
+++ issue_tests/tests/issue_000031.rs
@@ -0,0 +1,75 @@
+// Copyright © 2025 Tobias J. Prisching <tobias.prisching@icloud.com> and CONTRIBUTORS
+// including those who posted code snippets and/or image files for debugging
+// and testing purposes to the respective issue on GitHub.
+// See https://github.com/TechnikTobi/little_exif#license for licensing details
+
+/*
+Original problem:
+thread 'main' panicked at C:\Users\X\.cargo\registry\src\index.crates.io-6f17d22bba15001f\little_exif-0.6.0-beta.1\src\ifd\mod.rs:263:21:
+assertion `left == right` failed
+  left: Some(1313426255)
+ right: None
+*/
+
+/*
+Solved:
+Removed assert, see the following commit for more details:
+- eed3784c7c091ec590ffca71622439fd7a64a4fd
+*/
+
+use std::path::Path;
+
+extern crate little_exif_0_6_0_beta_1;
+extern crate little_exif_0_6_0_beta_2;
+extern crate little_exif;
+
+#[test]
+#[should_panic (expected = "assertion `left == right` failed\n  left: Some(1313426255)\n right: None")]
+fn
+read_exif_data_fails()
+{
+    let path = Path::new("resources/issue_000031/DSC22278.JPG");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif_0_6_0_beta_1::metadata::Metadata::new_from_path(path).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 47);
+}
+
+#[test]
+fn
+read_exif_data_fixed()
+{
+    let path = Path::new("resources/issue_000031/DSC22278.JPG");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif_0_6_0_beta_2::metadata::Metadata::new_from_path(path).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    assert_eq!(tag_counter, 47);
+}
+
+#[test]
+fn
+read_exif_data_current()
+{
+    let path = Path::new("resources/issue_000031/DSC22278.JPG");
+
+    let mut tag_counter = 0;
+
+    for _ in &little_exif::metadata::Metadata::new_from_path(path).unwrap()
+    {
+        tag_counter += 1;
+    }
+
+    // For some reason, there is now an additional tag found?
+    // No idea when this changed
+    assert_eq!(tag_counter, 48);
+}
