3514d222ee1c6aebb34705e4cfaabd91605b86d3
Tobias Prisching <tobias.prisching@icloud.com>
2025-11-14 00:17:58 +0100

    Fixed warnings

--- heif_conformance_tests/tests/heif_main.rs
+++ heif_conformance_tests/tests/heif_main.rs
@@ -6,14 +6,10 @@
 use std::fs::copy;
 use std::fs::remove_file;
 use std::path::Path;
-use std::str::FromStr;
 
 use test_case::test_case;
 
 extern crate little_exif;
-use little_exif::exif_tag::ExifTag;
-use little_exif::metadata::Metadata;
-use little_exif::filetype;
 
 fn 
 construct_cpy_path_string
