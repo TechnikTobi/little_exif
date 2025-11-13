e55515c4af1df2a0b5994df468d2774a60d0ab63
Tobias Prisching <tobias.prisching@icloud.com>
2025-11-13 15:09:26 +0100

    Download resources for heif conformance tests

---
+++ heif_conformance_tests/download.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C000.heic does not exist
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C001.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C002.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C003.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C004.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C005.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C006.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C007.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C008.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C009.heic
+
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C010.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C011.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C012.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C013.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C014.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C015.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C016.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C017.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C018.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C019.heic
+
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C020.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C021.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C022.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C023.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C024.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C025.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C026.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C027.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C028.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C029.heic
+
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C030.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C031.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C032.heic
+# wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C033.heic does not exist as of 2025-11-13
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C034.heic
+# wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C035.heic does not exist as of 2025-11-13
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C036.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C037.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C038.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C039.heic
+
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C040.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C041.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C042.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C043.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C044.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C045.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C046.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C047.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C048.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C049.heic
+
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C050.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C051.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C052.heic
+wget https://raw.githubusercontent.com/nokiatech/heif_conformance/refs/heads/master/conformance_files/C053.heic
+
