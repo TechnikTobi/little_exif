ca44a4f6d814abe9ae821188cf5e6964bcc44ce2
Rafał Mikrut <mikrutrafal@protonmail.com>
2026-01-09 18:22:35 +0100

    Older versions

--- issue_tests/tests/issue_000036.rs
+++ issue_tests/tests/issue_000036.rs
@@ -17,45 +17,6 @@ There was a typo, see related commit:
 
 use std::path::Path;
 
-extern crate little_exif_0_6_0_beta_3;
-extern crate little_exif_0_6_0_beta_4;
-extern crate little_exif;
-
-#[test]
-#[should_panic (expected = "assertion `left == right` failed\n  left: 0\n right: 58")]
-fn
-read_exif_data_fails()
-{
-    let path = Path::new("resources/issue_000036/IMG-20180904-WA0002.jpg");
-
-    let mut tag_counter = 0;
-
-    for tag in &little_exif_0_6_0_beta_1::metadata::Metadata::new_from_path(path).unwrap()
-    {
-        tag_counter += 1;
-        println!("{:?}", tag);
-    }
-
-    assert_eq!(tag_counter, 58);
-}
-
-#[test]
-fn
-read_exif_data_fixed()
-{
-    let path = Path::new("resources/issue_000036/IMG-20180904-WA0002.jpg");
-
-    let mut tag_counter = 0;
-
-    for tag in &little_exif_0_6_0_beta_4::metadata::Metadata::new_from_path(path).unwrap()
-    {
-        tag_counter += 1;
-        println!("{:?}", tag);
-    }
-
-    assert_eq!(tag_counter, 0);
-}
-
 #[test]
 #[should_panic (expected = "called `Result::unwrap()` on an `Err` value: Custom { kind: Other, error: \"No EXIF data found!\" }")]
 fn
